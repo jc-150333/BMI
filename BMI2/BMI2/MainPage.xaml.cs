296077a0033d10eba51594257c42c0df154e0333
150333 <150333@jc-21.jp>
2017-11-09 16:06:32 +0900

    初回

--- BMI2/BMI2/MainPage.xaml.cs
+++ BMI2/BMI2/MainPage.xaml.cs
@@ -13,5 +13,38 @@ namespace BMI2
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double h = Double.Parse(Height.Text);
+            Double w = Double.Parse(Weight.Text);
+
+            String B = "";
+
+            //DisplayAlert("身長", h.ToString(), "OK");
+            //DisplayAlert("体重", w.ToString(), "OK");
+
+            if (h >= 3)
+            {
+                h = h / 100;
+            }
+
+            Double BMI = w / (h * h);
+
+            if (BMI >= 30)
+            {
+                B = "肥満";
+            }
+            else if (BMI >= 18.5)
+            {
+                B = "普通体重";
+            }
+            else if (BMI <= 16)
+            {
+                B = "痩せ";
+            }
+
+            DisplayAlert("BMI", BMI.ToString() + "\n" + B + "です", "OK");
+        }
     }
 }
